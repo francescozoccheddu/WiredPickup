fce20ee178c1eea2d90d17a616452845976a3d92
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2018-11-23 13:42:58 +0100

    Add textureresource

--- Game/Sources/Include/Game/Resources/TextureResource.hpp
+++ Game/Sources/Include/Game/Resources/TextureResource.hpp
@@ -1,3 +1,54 @@
 #pragma once
 
 #include <Game/Resources/Resource.hpp>
+
+class TextureResource1D : public SimpleResource<ID3D11Texture1D>
+{
+
+public:
+
+	using SimpleResource::GetPointer;
+
+	bool bInitialize { false };
+	D3D11_TEXTURE1D_DESC description;
+	D3D11_SUBRESOURCE_DATA initialData;
+
+private:
+
+	virtual ID3D11Texture1D * Create (ID3D11Device & device) const override final;
+
+};
+
+class TextureResource2D : public SimpleResource<ID3D11Texture2D>
+{
+
+public:
+
+	using SimpleResource::GetPointer;
+
+	bool bInitialize { false };
+	D3D11_TEXTURE2D_DESC description;
+	D3D11_SUBRESOURCE_DATA initialData;
+
+private:
+
+	virtual ID3D11Texture2D * Create (ID3D11Device & device) const override final;
+
+};
+
+class TextureResource3D : public SimpleResource<ID3D11Texture3D>
+{
+
+public:
+
+	using SimpleResource::GetPointer;
+
+	bool bInitialize { false };
+	D3D11_TEXTURE3D_DESC description;
+	D3D11_SUBRESOURCE_DATA initialData;
+
+private:
+
+	virtual ID3D11Texture3D * Create (ID3D11Device & device) const override final;
+
+};
